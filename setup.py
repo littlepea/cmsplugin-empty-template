8fa9452fc13a96090f7f7678bdc07e9937598a75
littlepea <little_pea@list.ru>
2013-03-15 22:00:29 +0800

    Fixed setup.py and utils

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ import os
 
 from setuptools import find_packages, setup
 
-README = open('README.md').read()
+README = open('README.rst').read()
 
 setup(
     name='cmsplugin-',
     version=__import__('cmsplugin_').__version__,
     author='...',
     author_email='...@gmail.com',
-    url='http://github.com/...',
     description = ('django-cms plugin template to quickly create your own custom plugins.'),
     long_description = README,
     license = "BSD",
